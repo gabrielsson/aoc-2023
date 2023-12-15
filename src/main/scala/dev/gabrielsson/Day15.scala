c446feb2e9a293cbe0f619210ef78c90109f075f
mgabrielsson <max.gabrielsson@recordedfuture.com>
2023-12-14 23:01:04 -0800

    Func order
    Signed-off-by: mgabrielsson <max.gabrielsson@recordedfuture.com>

--- src/main/scala/dev/gabrielsson/Day15.scala
+++ src/main/scala/dev/gabrielsson/Day15.scala
@@ -1,36 +1,21 @@
 package dev.gabrielsson
 
+import dev.gabrielsson.Instruction.hashAlgo
+
 class Day15 extends Inputs {
 
-  case class Instruction(label: String, operation: Char, focalLength: Int = 0) {
-    def getBox() = hashAlgo(label.toCharArray)
-  }
-
   def part1(input: String): Long =
-    input.split(",").map(_.toCharArray)
-      .map(hashAlgo).sum
+    input
+      .split(",")
+      .map(Instruction.hashAlgo).sum
 
-  def hashAlgo(chars: Array[Char]): Int =
-    chars.foldLeft(0)((acc, c) => {
-      var currentValue = acc + c
-      currentValue *= 17
-      currentValue = currentValue % 256
-      currentValue
-    })
-
-  def part2(input: String): Int =
+  def part2(input: String): Int = {
     input.split(",")
-      .map(toInstruction)
-      .foldLeft(Map[Int, List[Instruction]]().withDefaultValue(List.empty))(applyInstruction).toList
+      .map(Instruction.fromString)
+      .foldLeft(Map[Int, List[Instruction]]().withDefaultValue(List.empty))((boxes, instr) => instr(boxes))
       .flatMap { case (boxId, values) =>
         calculateFinalScore(boxId, values)
       }.sum
-
-  private def applyInstruction(boxes: Map[Int, List[Instruction]], instr: Instruction) = {
-    instr.operation match {
-      case '-' => minusOperation(boxes, instr)
-      case _ => equalsOperation(boxes, instr)
-    }
   }
 
   private def calculateFinalScore(boxId: Int, values: List[Instruction]): List[Int] = {
@@ -38,34 +23,65 @@ class Day15 extends Inputs {
       (boxId + 1) * (index + 1) * value.focalLength
     }
   }
+}
 
-  private def toInstruction(chars: String) = {
+trait Instruction {
+  def label: String
+
+  def operation: Char
+
+  def focalLength: Int
+
+  def boxId: Int
+
+  def apply(boxes: Map[Int, List[Instruction]]): Map[Int, List[Instruction]]
+}
+
+object Instruction {
+  def hashAlgo(str: String): Int =
+    str.foldLeft(0)((currentValue, c) => ((currentValue + c) * 17) % 256)
+
+  def fromString(chars: String): Instruction = {
     if (chars.contains("-")) {
       val ns = chars.split("-")
-      Instruction(ns(0), '-')
+      MinusInstruction(ns(0))
     } else {
       val split = chars.indexOf("=")
-      Instruction(chars.substring(0, split), '=', chars.substring(split + 1).toInt)
+      EqualInstruction(chars.substring(0, split), chars.substring(split + 1).toInt)
     }
   }
+}
 
-  private def minusOperation(boxes: Map[Int, List[Instruction]], instr: Instruction) = {
-    val n = boxes(instr.getBox()).filter(_.label != instr.label)
-    boxes + (instr.getBox() -> n)
+case class EqualInstruction(label: String, focalLength: Int) extends Instruction {
+  val operation: Char = '='
+
+  override def boxId: Int = hashAlgo(label)
+
+  override def apply(boxes: Map[Int, List[Instruction]]): Map[Int, List[Instruction]] = {
+    boxes.updatedWith(boxId) {
+      case Some(existingInstructions) =>
+        val index = existingInstructions.indexWhere(_.label == label)
+        if (index >= 0) {
+          Some(existingInstructions.updated(index, this))
+        } else {
+          Some(existingInstructions :+ this)
+        }
+      case None =>
+        Some(List(this))
+    }
   }
+}
 
-  private def equalsOperation(boxes: Map[Int, List[Instruction]], instr: Instruction) = {
-    val m: List[Instruction] = boxes(instr.getBox())
-    if (m.map(_.label).contains(instr.label)) {
-      val i = m.find(_.label == instr.label).get
-      val idx = m.indexOf(i)
-      val tuple = m.splitAt(idx)
-      val n = (tuple._1 :+ instr) ++ tuple._2.tail
-      boxes + (instr.getBox() -> n)
-    } else {
-      val n: List[Instruction] = m :+ instr
-      val v: Map[Int, List[Instruction]] = (boxes + (instr.getBox() -> n))
-      v
+case class MinusInstruction(label: String) extends Instruction {
+  val operation: Char = '-'
+  val focalLength: Int = 0
+
+  override def boxId: Int = hashAlgo(label)
+
+  override def apply(boxes: Map[Int, List[Instruction]]): Map[Int, List[Instruction]] = {
+    boxes.updatedWith(boxId) {
+      case Some(existingInstructions) => Some(existingInstructions.filter(_.label != label))
+      case None => None
     }
   }
 }
